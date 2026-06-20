9b377f195c7db8c1e3626553b0fab82dd6ffff15
Syeda Eman Fatima <imanfatima0802@gmail.com>
2026-06-20 11:18:22 +0500

    chatbot initial code

---
+++ streamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
+
+
+st.title("Hello Streamlit!")
+st.write("This is a simple Streamlit app.")
+st.slider("Select Pay Range",0,100,50)
+st.button("Click Me!")
+st.subheader("Chatbot Form")
+user_input = st.text_input("Enter your query:")
+
+if st.button("Submit"):
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',  contents=user_input)
+
+    st.write(response.text)
